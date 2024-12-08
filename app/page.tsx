05292463552b558cb9b0590110e64503dac637ea
Aleksei Filonov <alexfiloonov@gmail.com>
2024-12-08 20:34:38 +0200

    Fix other dark:

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Index() {
     <>
       <main className="min-h-screen flex flex-col items-center">
         <div className="flex-1 w-full flex flex-col gap-20 items-center dot-background">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
           <div className="flex z-20 flex-col gap-20 w-[75%] px-5 pb-5">
             <HeaderHome />
