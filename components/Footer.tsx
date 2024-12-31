b147484f357fb5473ee6299f3a9b7e7a46737aa8
Aleksei Filonov <alexfiloonov@gmail.com>
2024-12-31 11:25:06 +0200

    Change Footer design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,41 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
+import { MdOutlineEmail } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <footer className="max-w-3xl mx-auto text-text_secondary pt-12 pb-4 px-5 md:px-0">
-      <h3 className="font-bold text-sm text-neutral-500 mb-2">LEGAL</h3>
-      <ul className="mb-8 space-y-2">
-        <li>
+    <footer className="max-w-7xl mx-auto text-text_secondary py-8 px-5 md:px-0">
+      <ul className="flex gap-2 flex-col md:flex-row md:justify-between">
+        <li className="space-x-4 flex justify-center">
           <Link
-            className="text-neutral-400 font-medium text-sm hover:text-neutral-200 duration-200"
+            className="text-neutral-400 font-medium hover:font-semibold hover:text-neutral-200 duration-200"
             href="/legal/privacy-policy"
           >
-            Privacy policy
+            Privacy
+          </Link>
+          <Link
+            className="text-neutral-400 font-medium hover:font-semibold hover:text-neutral-200 duration-200"
+            href="/legal/privacy-policy"
+          >
+            Terms
           </Link>
         </li>
-        <li>
+
+        <li className="flex justify-center">
+          <p className=" text-neutral-400 font-medium">
+            &copy; 2024 Timeliner. All rights reserved.
+          </p>
+        </li>
+        <li className="flex justify-center px-12">
           <Link
-            className="text-neutral-400 font-medium text-sm hover:text-neutral-200 duration-200"
-            href="/legal/terms-of-service"
+            className="text-neutral-400 font-medium hover:font-semibold hover:text-neutral-200 duration-200"
+            href="mailto:alexfiloonov@gmail.com"
           >
-            Terms of service
+            <MdOutlineEmail size={25} />
           </Link>
         </li>
       </ul>
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-xs text-neutral-400">
-          &copy; 2024 Timeliner. All rights reserved.
-        </p>
-      </div>
+      <div className="container mx-auto px-4 text-center"></div>
     </footer>
   );
 };
