820729f9390f698a20574a29e0df5bc1d00a2f63
Aleksei Filonov <alexfiloonov@gmail.com>
2025-01-02 12:36:11 +0200

    Test google analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { Suspense } from "react";
 import LoadingPage from "./[slug]/loading";
-
 import { getServerSession } from "next-auth";
 import SessionProvider from "@/utils/SessionProvider";
 import Head from "next/head";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -27,11 +26,28 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession();
+  const MEASUREMENT_ID = "G-P8J0Q1WGFK";
+  const GTM_ID = "GTM-58NBRQJ7";
 
   return (
     <html lang="en" className={GeistSans.className} suppressHydrationWarning>
+      <GoogleAnalytics GA_MEASUREMENT_ID={MEASUREMENT_ID} />
+
       <Head>
-        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LfrUqsqAAAAACeaP4W912fKNQlzSsaMMG3Tq4Pm"></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${MEASUREMENT_ID}');
+            `,
+          }}
+        />
       </Head>
 
       <SessionProvider session={session}>
---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import Script from "next/script";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { pageview } from "@/utils/gtagHelper";
+
+function GoogleAnalyticsComponent({
+  GA_MEASUREMENT_ID,
+}: {
+  GA_MEASUREMENT_ID: string;
+}) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+
+    pageview(GA_MEASUREMENT_ID, url);
+  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('consent', 'default', {
+                  'analytics_storage': 'denied',
+                  'ad_storage': 'denied',
+                  'ad_user_data': 'denied',
+                  'ad_personalization': 'denied',
+                  'personalization_storage': 'denied',
+                });
+                
+                gtag('config', '${GA_MEASUREMENT_ID}');
+                `,
+        }}
+      />
+    </>
+  );
+}
+
+export default function GoogleAnalyticsWrapper({
+  GA_MEASUREMENT_ID,
+}: {
+  GA_MEASUREMENT_ID: string;
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GoogleAnalyticsComponent GA_MEASUREMENT_ID={GA_MEASUREMENT_ID} />
+    </Suspense>
+  );
+}
